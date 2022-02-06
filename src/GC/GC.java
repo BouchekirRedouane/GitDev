a3a548264ae1dde965dd8212a4d344dbc0777bee
redOne <redouane.bouchekir@gmail.com>
2022-02-06 09:18:19 +0100

    commit dev one branch

---
+++ src/GC/GC.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GC;
+
+/**
+ *
+ * @author redouane
+ */
+public class GC {
+    
+    public GC(){
+         System.out.println("Dev 1");
+    }
+            
+    
+    
+}
